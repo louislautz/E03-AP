4d24dca6f206f74f160b9e84a41e16832f3a995e
louislautz <louislautz@gmx.net>
2021-10-24 21:27:25 +0300

    Removed DjangoKey from settings file for security reasons

--- crazy_book_club/crazy_book_club/settings.py
+++ crazy_book_club/crazy_book_club/settings.py
@@ -19,8 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+f=open("/djangoKey.txt","r")
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0tmr(d05#scbjn0aqb*wiuv81uh5m#h9wg6fzz)#&c_jyji4vw'
+SECRET_KEY = f.readlines()
+f.close()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
